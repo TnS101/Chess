0d578a3fdbaa9f6185045dcdff9637e8c4882d43
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-27 22:20:42 +0300

    Add Player Functionality

--- Card Game Engine/models/player.js
+++ Card Game Engine/models/player.js
@@ -1,7 +1,7 @@
 const rng = require('../utilities/rng');
 
 module.exports = class Player {
-    constructor(name, deck, startHandSize, maxHandSize, yourTurn, canSwapHand, health, mana, maxHealth, maxMana) {
+    constructor(name, deck, startHandSize, maxHandSize, yourTurn, fieldMaxLength, canSwapHand, health, mana, maxHealth, maxMana) {
         if (startHandSize >= maxHandSize) {
             throw new Error(`Starting Hand Size must not be more or equal to the Maximum Hand Size! (${startHandSize} < ${maxHandSize})`);
         }
@@ -9,14 +9,19 @@ module.exports = class Player {
         this.name = name;
         this.deck = deck;
         this.hand = [];
+        this.field = [];
+
         this.maxHandSize = maxHandSize;
         this.yourTurn = yourTurn;
-        this.canSwapHand = canSwapHand
+        this.fieldLength = 0;
+        this.fieldMaxLength = fieldMaxLength;
+        this.canSwapHand = canSwapHand;
 
         this.health = health;
         this.mana = mana;
         this.maxHealth = maxHealth;
         this.maxMana = maxMana;
+        this.isWinner = false;
 
         this.draw(startHandSize);
     }
@@ -52,5 +57,24 @@ module.exports = class Player {
 
             this.draw(amount);
         }
+    }
+
+    placeCard(name, position) {
+        if (this.fieldLength < this.fieldMaxLength) {
+            const card = this.hand.find(c => c.name === name);
+
+            if (card.manaCost <= this.mana) {
+                this.mana -= card.manaCost;
+
+                this.hand.splice(this.hand.indexOf(card), 1);
+                this.field[position] = card;
+                this.fieldMaxLength++;
+            }
+        }
+    }
+
+    giveUp(opponent) {
+        opponent.isWinner = true;
+        console.log(`${opponent.name} has won the Game!`);
     }
 }
